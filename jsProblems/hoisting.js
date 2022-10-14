9ef7d1ecbb63a60771121f25664415ba018581e4
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-10-15 00:23:34 +0500

    tried hoisting

---
+++ jsProblems/hoisting.js
@@ -0,0 +1,10 @@
+
+// hoisting in Javascript
+let a="hello";
+let arrow=()=>{
+const a=10;
+return console.log(a)
+//   a will be 10 as the scope is defined here as 10
+}
+// a will be hello as the global scope suggests 
+console.log(a)
