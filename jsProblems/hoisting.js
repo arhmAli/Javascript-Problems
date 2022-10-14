348a6549dba95dac9f14e2379b0ed860849c3e67
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-10-15 04:49:53 +0500

    Update hoisting

--- jsProblems/hoisting.js
+++ jsProblems/hoisting.js
@@ -6,5 +6,7 @@ const a=10;
 return console.log(a)
 //   a will be 10 as the scope is defined here as 10
 }
+arrow() 
 // a will be hello as the global scope suggests 
 console.log(a)
+a===10?console.log("inside function") :return Console.log("outside");
