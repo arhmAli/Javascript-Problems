faecaf4d9526d946fc24488bca160537f9df304c
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-10-25 04:58:32 +0500

    created problems

---
+++ jsProblems/Problems.js
@@ -0,0 +1,435 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0
+};
+const Changer = createSlice({
+  name: "changer",
+  initialState,
+  reducers: {
+    addNum: (state) => {
+      return state.value + 1;
+    }
+  }
+});
+export const { addNum } = Changer.actions;
+export default Changer.reducer;
