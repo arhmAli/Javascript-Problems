566bf6c0a27cc38e60c5a6854bb93af906cf6037
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-11-05 00:06:41 +0500

    update

--- jsProblems/arrow.js
+++ jsProblems/arrow.js
@@ -2,7 +2,7 @@ const arrow = (name)=>{
 console.log(`You have entered ${name}` )
 }
 alert(`You have entered ${arrow()}`)
-const arrowSecond=()=>{
-  console.log("another one")
+const arrowSecond=(something)=>{
+  console.log(`another one ${something}`)
 }
-arrowSecond();
+arrowSecond(lesgoooo);
