0c7ef4c7f64d7d789b434f5e975087023f3a08bd
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-11-06 12:54:03 +0500

    update

--- jsProblems/practiceThree.js
+++ jsProblems/practiceThree.js
@@ -1,5 +1,7 @@
 //some random code
 let array=[1,2,3,4,5,6,7,8,9,10];
+let anotherNum=array.map(item()=>{
+return array[item]+1})
 let maximum=Math.max(array);
 let minimum=Math.min(array);
 let ranNum=Math.floor(Math.random*maximun)+1;
@@ -7,4 +9,5 @@ let here=minimum+maximum;
 console.log(here);
 let someNum=ranNum+maximum;
 someNum ===1? console.log("nah"):return Null;
+console.log(anotherNum);
 //end
