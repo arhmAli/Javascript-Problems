e7be57059dc92115ce7eb35334b0c5ef168f48d7
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-11-06 11:09:16 +0500

    update

--- jsProblems/practiceThree.js
+++ jsProblems/practiceThree.js
@@ -1,5 +1,5 @@
 //some random code
-let array=[1,2,3,4,5];
+let array=[1,2,3,4,5,6,7,8,9,10];
 let maximum=Math.max(array);
 let minimum=Math.min(array);
 let ranNum=Math.floor(Math.random*maximun)+1;
