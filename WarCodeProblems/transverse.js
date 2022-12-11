b6808e7a8fcead173880d2e30408e11d94096a82
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-11 10:00:41 +0500

    created transverseArray

---
+++ WarCodeProblems/transverse.js
@@ -0,0 +1,14 @@
+let input=[2,4,6,8,10];
+
+let out=input.filter((item,ind)=>item(ind));
+
+let someValue=2;
+for (i=0;i<input.length;i++)
+{
+console.log(input[i];
+ input[i].value===6?console.log("You have given 6"):null
+}
+for(i=0;i<input.length;i++){
+input[someValue]==input[someValue]+1
+console.log(i)
+}
