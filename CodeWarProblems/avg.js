632ab55ac2e7c1f812a086ff1ff8c4fdf83333f9
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-17 07:16:36 +0500

    created avg

---
+++ CodeWarProblems/avg.js
@@ -0,0 +1,10 @@
+function findAverage(array) {
+  // your code here
+  let sum=0;
+
+
+  let newSum=array.reduce((curr,prev)=>curr+prev,sum)
+  let den=array.length;
+  let average=newSum/den;
+  return den===0?0:average;
+}
