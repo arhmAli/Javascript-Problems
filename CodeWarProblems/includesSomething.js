eb613a6f45a69f6e14d5bb48e69803741f1cd107
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-03 13:28:58 +0500

    Given array contains x

--- CodeWarProblems/includesSomething.js
+++ CodeWarProblems/includesSomething.js
@@ -3,6 +3,23 @@
 //Array can contain numbers or strings. X can be either.
 
 //Return true if the array contains the value, false if not.
+
+
 function check(a, x) {
   return a.includes(x)?true:false
 }
+
+
+//Test cases
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(check([66, 101], 66), true);
+    assert.strictEqual(check([101, 45, 75, 105, 99, 107], 107), true);
+    assert.strictEqual(check(['t', 'e', 's', 't'], 'e'), true);
+    assert.strictEqual(check(['what', 'a', 'great', 'kata'], 'kat'), false);
+  })
+})
