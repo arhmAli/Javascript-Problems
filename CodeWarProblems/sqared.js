997318e13511936ee5e1757f2b2d1f87dfca16fb
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-03 13:23:30 +0500

    statement

--- CodeWarProblems/sqared.js
+++ CodeWarProblems/sqared.js
@@ -1,7 +1,8 @@
-// const squared=(arr)=>(
-//   arr.map((val)=>(
-//   val*val))
-// )
+const squared=(arr)=>(
+  let mapped=arr.map((val)=>(
+  val*val))
+   return mapped
+)
 
 // This is a simple problem in which you have an array of numbers and you return the numbers sqaured in array
 
@@ -9,7 +10,9 @@
 
 // all entries are positive numbers
 
-//
+//[2,3,4] => [4,9,16]
+
+
 function(arr){
 let mapped=arr.map((val)=>(val*val))
 return mapped
