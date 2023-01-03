6c079f406e009e3072687fe4ae7c2b4df8e2a002
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-03 13:24:16 +0500

    square of all entries in array

--- CodeWarProblems/sqared.js
+++ CodeWarProblems/sqared.js
@@ -1,8 +1,11 @@
+// Practing for arrow functions
+
 const squared=(arr)=>(
   let mapped=arr.map((val)=>(
   val*val))
    return mapped
 )
+//if the syntax is not correct 
 
 // This is a simple problem in which you have an array of numbers and you return the numbers sqaured in array
 
@@ -12,6 +15,7 @@ const squared=(arr)=>(
 
 //[2,3,4] => [4,9,16]
 
+//For regular function 
 
 function(arr){
 let mapped=arr.map((val)=>(val*val))
