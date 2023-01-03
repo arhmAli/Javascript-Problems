8a6ca89c435cd15ec927050fffd02a8c4488d2c7
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-03 13:22:38 +0500

    statem

--- CodeWarProblems/sqared.js
+++ CodeWarProblems/sqared.js
@@ -3,7 +3,13 @@
 //   val*val))
 // )
 
-// This is a simple problem in which you have an array of numbers
+// This is a simple problem in which you have an array of numbers and you return the numbers sqaured in array
+
+// Test cases assumptions 
+
+// all entries are positive numbers
+
+//
 function(arr){
 let mapped=arr.map((val)=>(val*val))
 return mapped
