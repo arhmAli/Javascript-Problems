ee6b17697f77417dee9589fe265c983934f1ad1f
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-03 13:21:09 +0500

    of all element of array

---
+++ CodeWarProblems/sqared.js
@@ -0,0 +1,10 @@
+// const squared=(arr)=>(
+//   arr.map((val)=>(
+//   val*val))
+// )
+
+// This is a simple problem in which 
+function(arr){
+let mapped=arr.map((val)=>(val*val))
+return mapped
+}
