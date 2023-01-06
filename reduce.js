c4c97170cff32426a09f2f2f0a24d80d8dfa141c
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-06 16:39:24 +0500

    update

--- reduce.js
+++ reduce.js
@@ -1,2 +1,9 @@
 //simple program illustrating reduce method of array
 //lets say i have a array of objects or any array
+//For simplicity we will have an array of numbers.
+
+let num=[1,2,3,4,5]
+
+//lets solve this array to find out the sum of the array using reduce method.
+
+
