e7ddbf9cc4e8609c0002c8c72edff98379e40d52
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-07 12:11:06 +0500

    found the area | per of given prob

--- CodeWarProblems/area|per.js
+++ CodeWarProblems/area|per.js
@@ -1,3 +1,10 @@
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// Example(Input1, Input2 --> Output):
+
+// 6, 10 --> 32
+// 3, 3 --> 9
 const areaOrPerimeter = function(l , w) {
   //let per=2*(l+w)
   return l===w?l*w:2*(l+w)
