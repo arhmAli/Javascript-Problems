ecc5a5d59307d90e312b36d44370695f99e6bc9a
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-09 13:39:09 +0500

    solved for two smallest in array

--- CodeWarProblems/sumSmallest.js
+++ CodeWarProblems/sumSmallest.js
@@ -1,3 +1,7 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
 const sumTwoSmallestNumbers=(numbers) => (  
   let sorted=numbers.sort((a,b)=>{
     if (a > b)
