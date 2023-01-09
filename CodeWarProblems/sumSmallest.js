c83ec093f8c142c40687b494de41c8e124f7fff7
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-09 13:37:38 +0500

    ad

---
+++ CodeWarProblems/sumSmallest.js
@@ -0,0 +1,12 @@
+function sumTwoSmallestNumbers(numbers) {  
+  let sorted=numbers.sort((a,b)=>{
+    if (a > b)
+        return 1;
+    if (a < b)
+        return -1;
+    return 0;
+  })
+ let newArray=numbers.slice(0,2)
+ let sum=newArray.reduce((a,b)=>(a+b))
+  return sum
+}
