f5b1268d847e0664a24fb1a76dbdbe2d462f9cef
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-16 16:05:26 +0500

    made alphanumeric test regex

--- CodeWarProblems/regex_1.js
+++ CodeWarProblems/regex_1.js
@@ -9,3 +9,18 @@
 function alphanumeric(string){
   return /^[a-z0-9]+$/gi.test(string)?true:false
 }
+
+
+
+
+///TEST CASES
+const { assert } = require('chai');
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(alphanumeric("Mazinkaiser"), true)
+    assert.strictEqual(alphanumeric("hello world_"), false)
+    assert.strictEqual(alphanumeric("PassW0rd"), true)
+    assert.strictEqual(alphanumeric("     "), false)
+  });
+});
