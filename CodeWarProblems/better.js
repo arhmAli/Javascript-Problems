117856de9b668d70ee27aa2a67f115ae6599e764
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-16 09:59:52 +0500

    solved howBetterRU

--- CodeWarProblems/better.js
+++ CodeWarProblems/better.js
@@ -1,3 +1,5 @@
+//Problem statement
+
 There was a test in your class and you passed it. Congratulations!
 But you're an ambitious person. You want to know if you're better than the average student in your class.
 
@@ -8,6 +10,7 @@ Return True if you're better, else False!
 Note:
 Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array
 
+//Solution:
 
 function betterThanAverage(classPoints, yourPoints) {
   let sum=0;
@@ -15,3 +18,29 @@ function betterThanAverage(classPoints, yourPoints) {
   let avg=sum/classPoints.length;
   return avg>yourPoints?false:true
 }
+
+//Test cases
+const chai = require('chai');
+const assert = chai.assert;
+
+describe("Example Tests", function() {
+  it("betterThanAverage([2, 3], 5) should return True", function() {
+    assert.strictEqual(betterThanAverage([2, 3], 5), true);
+  });
+  
+  it("betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75) should return True", function() {
+    assert.strictEqual(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75), true);
+  });
+  
+  it("betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9) should return False", function() {
+    assert.strictEqual(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9), false);
+  });
+  
+  it("betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50) should return False", function() {
+    assert.strictEqual(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50), false);
+  });
+  
+  it("betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21) should return False", function() {
+    assert.strictEqual(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21), false);
+  });
+});
