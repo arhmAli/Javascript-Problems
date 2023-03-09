ee5addf4033ae1cee7dd2e01a79f4f33a0d2da98
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-03-10 00:05:47 +0500

    forgot the {}

--- CodeWarProblems/convertObj.js
+++ CodeWarProblems/convertObj.js
@@ -10,7 +10,7 @@ return convert
 We can also use a builtin function
 
 const convertorBuilt=(arr)=>{
-return Object.assign(arr)
+return Object.assign({},arr)
 }
 
 The object.assign will put the indices as default keys
