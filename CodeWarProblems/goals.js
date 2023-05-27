89314da0b9de2bb0f544ee78a7dc5ae87e8959e0
Arham Khan <arhamkhank353@gmail.com>
2023-05-27 16:11:09 +0500

    Create goals.js

---
+++ CodeWarProblems/goals.js
@@ -0,0 +1,27 @@
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+return laLigaGoals+copaDelReyGoals+championsLeagueGoals
+}
+
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(goals(0,0,0), 0)
+Test.assertEquals(goals(43, 10, 5), 58)
+  });
+});
