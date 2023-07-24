29ec88a72fdfeb4e5d0442cd902de55da81727c4
Arham Khan <arhamkhank353@gmail.com>
2023-07-24 16:07:22 +0500

    Update fibnaunci.ts

--- fibnaunci.ts
+++ fibnaunci.ts
@@ -1,7 +1,7 @@
 Write a function called 'nthFibonacci' that takes an integer n as an
 argument and returns the nth number in the Fibonacci sequence.
 
-
+// classical approach
 function fib(n:number){
   if(n<=1){
     return n
@@ -15,4 +15,12 @@ function fib(n:number){
     currNum=fibNext;
   }
   return currNum
+}
+
+// if used the recursive approach
+function fib(n:number){
+ if(n<=1){
+    return n
+  }
+  return fib(n-1) + fib(n-2)
 }
