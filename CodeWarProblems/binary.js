24c26c4676124728a9ab4755b23761aa3fd5839d
Arham Khan <arhamkhank353@gmail.com>
2023-09-16 11:16:46 +0500

    Create binary.js

---
+++ CodeWarProblems/binary.js
@@ -0,0 +1,8 @@
+// Given a non-negative integer n, 
+//   write a function to_binary/ToBinary
+//   which returns that number in a binary format
+
+function toBinary(n){
+  return parseInt(n.toString(2));
+   
+}
