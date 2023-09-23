8f510e148da4fb0c56b01793a06594f5ecb3e86a
Arham Khan <arhamkhank353@gmail.com>
2023-09-23 20:05:42 +0500

    Update descendingOrder.js

--- CodeWarProblems/descendingOrder.js
+++ CodeWarProblems/descendingOrder.js
@@ -7,3 +7,12 @@ function descendingOrder(n){
   let srt=arr.sort().reverse()
   return Number(srt.join(''))
 }
+
+//i retried after some days and this was my approach this time:
+function descendingOrder(n){
+  let arr=n.toString().split('')
+  let fil=arr.map((item)=>+(item))
+  let srt=fil.sort((a,b)=>b-a)
+  return +(srt.join(''))
+}
+//tells that u cant cram urself to be a programmer so learn to solve the programmer dont "remember how to solve a problem"
