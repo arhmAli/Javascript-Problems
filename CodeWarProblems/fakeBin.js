14680508c191e7d2d4f1316d886622d2efc87bdc
Arham Khan <arhamkhank353@gmail.com>
2023-09-26 13:04:17 +0500

    Create fakeBin.js

---
+++ CodeWarProblems/fakeBin.js
@@ -0,0 +1,17 @@
+// Given a string of digits,
+//  you should replace any digit below 5 with '0' and any 
+//   digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(x){
+let arr=x.split('')
+for(let i=0;i<arr.length;i++){
+  arr[i]=parseInt(arr[i])
+  if (arr[i]<5){
+    arr[i]=0
+  }
+  else{
+    arr[i]=1
+  }
+}
+  return arr.join('')
+}
